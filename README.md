97d647f5fa3e6608b13e3e8d5475ac78df4d342b
dalyzhee <dalmasodhiambolevi@gmail.com>
2023-11-01 22:49:22 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# A Facebook Login automation
+
+This is a simple python program that allows users to automate facebook login us selenium which is a python library 
+that automates and control browser and its activities.
+
+## Functionality
+1. Open Chrome browser
+2. finds the email, password elements from browser
+3. send keys
+4. Click login button
+
+## How to run 
+
+- Open commandline in the folder conatining the python file
+- execute `python app.py`
---
+++ app.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from getpass4 import getpass
+
+# email = input("Enter email: ")
+# password = getpass("Enter password: ")
+driver = webdriver.Chrome()
+driver.get("https://www.facebook.com/login/")
+print("Facebook opened")
+sleep(1)
+
+user_email = driver.find_element(By.ID, 'email_container')
+user_email.send_keys("levis@gmaol.com")
+print("Email Entered")
+sleep(1)
+user_password = driver.find_element(By.ID, 'pass')
+user_password.send_keys("leviso")
+print("Password Entered")
+
+login = driver.find_element(By.ID, 'loginbutton')
+login.click()
+
+print("Done")
+input("Enter any key to quit")
+driver.quit()
+print("finished")
